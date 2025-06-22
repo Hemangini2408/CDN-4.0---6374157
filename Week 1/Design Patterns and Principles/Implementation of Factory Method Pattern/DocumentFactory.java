90389a6330028c4526198c3825674a9af814278d
Hemangini <hemanginisenapati@gmail.com>
2025-06-22 21:40:00 +0530

    Update DocumentFactory.java

--- Week 1/Design Patterns and Principles/Implementation of Factory Method Pattern/DocumentFactory.java
+++ Week 1/Design Patterns and Principles/Implementation of Factory Method Pattern/DocumentFactory.java
@@ -1,14 +1,3 @@
-public class DocumentFactory {
-    public static Document getDocument(DocumentType type) {
-        switch (type) {
-            case WORD:
-                return new DocumentImpl.WordDocument();
-            case PDF:
-                return new DocumentImpl.PdfDocument();
-            case EXCEL:
-                return new DocumentImpl.ExcelDocument();
-            default:
-                throw new IllegalArgumentException("Unsupported document type.");
-        }
-    }
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
 }
